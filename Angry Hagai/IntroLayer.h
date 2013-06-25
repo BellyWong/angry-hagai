d9731b733b5da707ff1e846c49ac6f24d0dd73ea
Adam <adam@Adams-Mac-mini.local>
2013-06-25 17:27:07 +0300

    Initial Commit

---
+++ Angry Hagai/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Angry Hagai
+//
+//  Created by Adam on 6/25/13.
+//  Copyright Adam 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
